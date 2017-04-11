f293000253b1ac37481945d2f76f1ca74f275e27
fujindong <alanfujindong@gmail.com>
2017-04-11 23:28:09 +0800

    add welcome

--- app/src/main/java/com/scottfu/brightbook/WelcomeActivity.java
+++ app/src/main/java/com/scottfu/brightbook/WelcomeActivity.java
@@ -1,8 +1,12 @@
 package com.scottfu.brightbook;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.scottfu.sflibrary.util.LogUtil;
 
 /**
  * Created by fujindong on 2017/4/11.
@@ -10,11 +14,28 @@ import android.support.v7.app.AppCompatActivity;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-
+    private String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                LogUtil.e(TAG,"----------------");
+                initBrightBook();
+            }
+
+        }, 2 * 1000);
+
+
+    }
+
+    private void initBrightBook() {
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
     }
 }
