b04488625b005de3f64dbf33024a4f4cad5dee22
fujindong <alanfujindong@gmail.com>
2017-04-13 21:53:58 +0800

    update

--- app/src/main/java/com/scottfu/brightbook/MainActivity.java
+++ app/src/main/java/com/scottfu/brightbook/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
